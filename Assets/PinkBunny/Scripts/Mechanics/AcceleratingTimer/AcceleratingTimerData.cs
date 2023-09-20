12f0a20e847327404ff063414504675bc6b81ac7
Laphed <a.gavrilushkin@virgroup.xyz>
2023-09-20 21:31:48 +0400

    feature(timer): add dto for timer

---
+++ Assets/PinkBunny/Scripts/Mechanics/AcceleratingTimer/AcceleratingTimerData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Laphed.Mechanics.AcceleratingTimer
+{
+    [Serializable]
+    public struct AcceleratingTimerData
+    {
+        public AnimationCurve curve;
+    }
+}
