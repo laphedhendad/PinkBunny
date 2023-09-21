300b47b45288e6ecc69ff54f5192bea1e36491bf
Laphed <a.gavrilushkin@virgroup.xyz>
2023-09-21 22:05:59 +0400

    feature(timer): add accelerating timer

--- Assets/PinkBunny/Scripts/Mechanics/AcceleratingTimer/AcceleratingTimer.cs
+++ Assets/PinkBunny/Scripts/Mechanics/AcceleratingTimer/AcceleratingTimer.cs
@@ -1,20 +1,78 @@
 ﻿using System;
+using System.Collections;
+using Laphed.Utils.CoroutineProvider;
+using Laphed.Utils.ExceptionsHandler;
 using UnityEngine;
 
 namespace Laphed.Mechanics.AcceleratingTimer
 {
-    public class AcceleratingTimer
+    public class AcceleratingTimer: IAcceleratingTimer
     {
-        public event Action<float> onTicked; 
-        public event Action onTimerEnd;
+        public event Action OnTimerEnd;
+        public event Action<float> OnTicked;
         
         private AnimationCurve curve;
+        private float currentTime;
+        private Coroutine timerCoroutine;
+        private float realDuration;
+        
+        private readonly ICoroutineProvider coroutineProvider;
 
-        public AcceleratingTimer(AcceleratingTimerData timerData)
+        public AcceleratingTimer(ICoroutineProvider coroutineProvider)
         {
-            curve = timerData.curve;
+            this.coroutineProvider = coroutineProvider;
+        }
+
+        public void Start(AnimationCurve curve)
+        {
+            this.curve = curve;
+            realDuration = curve.keys[^1].time;
+            currentTime = 0;
+            timerCoroutine = coroutineProvider.StartCoroutine(RealTimerCoroutine());
         }
         
-        
+        public void Stop()
+        {
+            if (timerCoroutine == null) return;
+            
+            coroutineProvider.StopCoroutine(timerCoroutine);
+            timerCoroutine = null;
+        }
+
+        public void Continue()
+        {
+            if (curve == null) throw new ContinueNotStartedTimerException();
+            if (timerCoroutine != null) throw new ContinueNotStoppedTimerException();
+
+            timerCoroutine = coroutineProvider.StartCoroutine(RealTimerCoroutine());
+        }
+
+        private void End()
+        {
+            OnTimerEnd?.Invoke();
+        }
+
+        private void Tick()
+        {
+            OnTicked?.Invoke(GetCurrentValue(currentTime));
+        }
+
+        private IEnumerator RealTimerCoroutine()
+        {
+            while (currentTime < realDuration)
+            {
+                currentTime += Time.deltaTime;
+                Tick();
+                
+                yield return new WaitForEndOfFrame();
+            }
+            
+            End();
+        }
+
+        private float GetCurrentValue(float realTime)
+        {
+            return curve.Evaluate(realTime);
+        }
     }
 }
