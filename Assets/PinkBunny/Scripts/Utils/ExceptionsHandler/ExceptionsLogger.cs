dbe43f826210d773eca505dfc5c0910bd5823d74
Laphed <a.gavrilushkin@virgroup.xyz>
2023-09-21 22:00:09 +0400

    feature(exceptions): add custom exceptions and logger

---
+++ Assets/PinkBunny/Scripts/Utils/ExceptionsHandler/ExceptionsLogger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Laphed.Utils.ExceptionsHandler
+{
+    public static class ExceptionsLogger
+    {
+        public static void Log(string exceptionText)
+        {
+            Debug.LogError(exceptionText);
+        }
+    }
+}
