8cd365690e0a49b25b82388aa9abb109a627b28b
Laphed <a.gavrilushkin@virgroup.xyz>
2023-09-22 20:15:23 +0400

    feature(core): add project context

---
+++ Assets/PinkBunny/Scripts/ProjectContextMonoInstaller.cs
@@ -0,0 +1,17 @@
+using Laphed.Utils.CoroutineProvider;
+using UnityEngine;
+using Zenject;
+
+public class ProjectContextMonoInstaller : MonoInstaller
+{
+    [SerializeField] private CoroutineProvider coroutineProvider;
+    public override void InstallBindings()
+    {
+        BindUtils();
+    }
+
+    private void BindUtils()
+    {
+        Container.Bind<CoroutineProvider>().FromInstance(coroutineProvider).AsSingle();
+    }
+}
