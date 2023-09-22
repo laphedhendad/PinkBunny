de69d29a4578f88ee4e8f999a187a882e0518b7e
Laphed <a.gavrilushkin@virgroup.xyz>
2023-09-22 21:14:13 +0400

    feature(level): add level completed/failed handler

---
+++ Assets/PinkBunny/Scripts/Mechanics/QTEBasedLevel/QTEBasedLevel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Laphed.Mechanics.LevelEventBus;
+using UnityEngine;
+using Zenject;
+
+namespace Laphed.Mechanics.QTEBasedLevel
+{
+    public class QTEBasedLevel: ILevelCompletedHandler, ILevelFailedHandler, IDisposable
+    {
+        private readonly IExitPointEventsRegistrar eventBus;
+        
+        [Inject]
+        public QTEBasedLevel(IExitPointEventsRegistrar eventBus)
+        {
+            this.eventBus = eventBus;
+            eventBus.SubscribeOnLevelCompleted(this);
+            eventBus.SubscribeOnLevelFailed(this);
+        }
+        
+        public void OnLevelCompleted()
+        {
+            Debug.Log("Level Completed");
+        }
+
+        public void OnLevelFailed()
+        {
+            Debug.Log("Level Failed");
+        }
+
+        public void Dispose()
+        {
+            eventBus.UnsubscribeLevelCompleted(this);
+            eventBus.UnsubscribeLevelFailed(this);
+        }
+    }
+}
---
+++ Assets/PinkBunny/Scripts/Mechanics/QTEBasedLevel/QTEBasedLevelInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Laphed.Mechanics.LevelEventBus;
+using Zenject;
+
+namespace Laphed.Mechanics.QTEBasedLevel
+{
+    public class QTEBasedLevelInstaller: MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            BindEventBus();
+        }
+
+        private void BindEventBus()
+        {
+            Container.BindInterfacesTo<EventBus>().AsSingle();
+        }
+    }
+}
