0e20a6824ee306efb289fdf222e435c4dd9ea94f
Laphed <a.gavrilushkin@virgroup.xyz>
2023-10-01 16:51:08 +0400

    feature(editor): add animation curve inspector extension

---
+++ Assets/PinkBunny/Scripts/Utils/AnimationCurveInspectorExtension/AnimationCurveExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Laphed.AnimationCurveInspectorExtension
+{
+    [Serializable]
+    public class AnimationCurveExtension
+    {
+        public AnimationCurve curve;
+#if UNITY_EDITOR
+        [SerializeField] private List<Vector2> keyPoints;
+#endif
+    }
+}
---
+++ Assets/PinkBunny/Scripts/Utils/AnimationCurveInspectorExtension/Editor/AnimationCurvePropertyDrawer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Laphed.AnimationCurveInspectorExtension
+{
+    [CustomPropertyDrawer(typeof(AnimationCurveExtension))]
+    public class AnimationCurvePropertyDrawer: PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            
+            SerializedProperty curveProperty = property.FindPropertyRelative("curve");
+            SerializedProperty pointsProperty = property.FindPropertyRelative("keyPoints");
+
+
+            AnimationCurve curve = new AnimationCurve();
+            
+            for (int i = 0; i < pointsProperty.arraySize; i++)
+            {
+                SerializedProperty pointProperty = pointsProperty.GetArrayElementAtIndex(i);
+                curve.AddKey(pointProperty.vector2Value.x, pointProperty.vector2Value.y);
+            }
+            
+            curveProperty.animationCurveValue = curve;
+            
+            EditorGUILayout.PropertyField(curveProperty, new GUIContent("Curve"));
+            EditorGUILayout.PropertyField(pointsProperty, new GUIContent("Points"), true);
+
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
