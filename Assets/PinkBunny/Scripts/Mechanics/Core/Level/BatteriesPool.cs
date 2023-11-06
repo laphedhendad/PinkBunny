a6233d1f8cec161643aef9c999afd08f5bb9954b
Laphed <a.gavrilushkin@virgroup.xyz>
2023-11-06 18:44:22 +0400

    feature(level): add interface to batteries pool

---
+++ Assets/PinkBunny/Scripts/Mechanics/Core/Level/BatteriesPool.cs
@@ -0,0 +1,15 @@
+﻿namespace Laphed.PinkBunny
+{
+    public class BatteriesPool: IBatteriesPool
+    {
+        public int Counter { get; private set; }
+
+        public void UseBattery()
+        {
+        }
+
+        public void SetBatteriesAmount(int amount)
+        {
+        }
+    }
+}
---
+++ Assets/PinkBunny/Scripts/Mechanics/Core/Level/IBatteriesPool.cs
@@ -0,0 +1,9 @@
+﻿namespace Laphed.PinkBunny
+{
+    public interface IBatteriesPool
+    {
+        int Counter { get; }
+        void UseBattery();
+        void SetBatteriesAmount(int amount);
+    }
+}
